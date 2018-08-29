d7959aa060d715bb1c468799c69f7372903f37ac
Diogo <diogoocordeiro@gmail.com>
2018-08-29 14:07:48 -0300

    v1.1

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -2,10 +2,11 @@ package model;
 
 import utils.Convert;
 
+import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(Convert.converter());
+    public static void main(String[] args) throws IOException {
+        System.out.println(Convert.converter("C:\\Users\\diogo.cordeiro\\Documents\\GitHub\\API-SCO\\JSON\\paises.xml"));
     }
 }
--- src/main/java/utils/Convert.java
+++ src/main/java/utils/Convert.java
@@ -3,21 +3,24 @@ package utils;
 import org.json.JSONObject;
 import org.json.XML;
 
-public class Convert {
-        public static JSONObject converter(){
-            String xml = "<?xml version=\"1.0\"?>\n" +
-                    "<paises>\n" +
-                    "   <pais sigla=\"BR\">\n" +
-                    "       <nome>Brasil</nome>\n" +
-                    "       <populacao>196.655.014</populacao>\n" +
-                    "   </pais>\n" +
-                    "   <pais sigla=\"AR\">\n" +
-                    "       <nome>Argentina</nome>\n" +
-                    "       <populacao>40.764.561</populacao>\n" +
-                    "   </pais>\n" +
-                    "</paises>";
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+
+
+public class Convert {
+
+            //Converte o String xml em JSON
+    public static JSONObject converter(String path) throws IOException {
+            String xml = readFile(path);
             JSONObject json = XML.toJSONObject(xml);
             return json;
         }
+
+    //Lê o arquivo xml
+    public static String readFile(String path) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded);
+    }
 }
